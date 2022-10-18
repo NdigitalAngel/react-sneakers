08a43d5bfacd7a0b53b2ca1450579c5d1cfb3ad0
Haki <defvermillion@gmail.com>
2022-10-18 18:02:08 +0300

    almost

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import Card from './components/Card';
-import Header from './components/Header';
-import Drawer from './components/Drawer';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    name: "Мужские кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+];
 
 function App() {
-  return <div className="wrapper clear">
-    <Drawer />
-    
+  return (
+    <div className="wrapper clear">
+      <Drawer />
 
+      <Header />
 
+      <div className="content p-40">
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
 
-    <Header />
-    <div className="content p-40">
-      <div className="d-flex align-center mb-40 justify-between">
-      <h1>Все кроссовки</h1>
-      <div className="search-block d-flex">
-        <img src="/img/search.svg" alt="search" />
-        <input placeholder="Поиск..." />
+        <div className="d-flex">
+          {arr.map((obj) => (
+            <Card title={obj.name} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
+        </div>
       </div>
-      </div>
-    
-
-      <div className="d-flex">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      </div>
-
-
     </div>
-  </div>
+  );
 }
 
 export default App;
-
-
-
-
-
